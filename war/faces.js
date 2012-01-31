0033cbe8e3df6dc30f974dd6304febda3c39b0da
Henric <henric.persson@gmail.com>
2012-01-31 20:36:30 +0100

    Added facebook connect

--- war/faces.js
+++ war/faces.js
@@ -1,16 +1,15 @@
 function Faces() {
 	var self = this;
-	var textarea, facesWrapper;
+	var facesWrapper;
 	var popup;
 	
 	
 	this.init = function() {
-		textarea = $('<textarea/>');
-		textarea.attr('value', '100000600820145, 100000609251324, 100000657789529, 100000708078257, 100001351865883, 100001865837268, 100001890039723, 100002221813932, 100003182545248');
 		var button = $('<button/>');
-		button.text('Show faces');
+		button.text('Connect to Facebook');
 		button.click(function() {
-			self.showFaces();
+			button.hide();
+			self.connectToFacebook();
 		});
 		facesWrapper = $('<div/>', {'class': 'faces'});
 		popup = $('<div/>', {'class': 'popup'});
@@ -20,44 +19,47 @@ function Faces() {
 		popup.mouseleave(function() {
 			popup.hide();
 		});
-		$('body').append(textarea).append(button).append(facesWrapper).append(popup);
+		$('body').append(button).append(facesWrapper).append(popup);
 	};
-	
-	
-	this.showFaces = function() {
-		facesWrapper.empty();
-		var facebookIds = textarea.attr('value');
-		var split = facebookIds.split(',');
-		for ( var i = 0; i < split.length; i++) {
-			self.showFace(split[i].trim());
-		}
-	};
-	
-	this.showPopup = function(fbId) {
-		$.ajax({
-			  url: 'https://graph.facebook.com/' + fbId,
-			  dataType: 'jsonp',
-			  success: function(json) {
-					popup.find('a').text(json.name).attr('href', 'http://www.facebook.com/profile.php?id=' + fbId);
-				  	
-					var relativeTo = $('.fb-' + fbId);
-					var relativeOffset = relativeTo.offset();
-					var width = relativeTo.width();
-					popup.css('top', relativeOffset.top);
-					popup.css('left', relativeOffset.left + width);
-					popup.show();
-			  }
-		}); 
+	this.connectToFacebook = function() {
+		FB.login(function(response) {
+		   if (response.authResponse) {
+		     FB.api('/me/friends', function(response) {
+		    	 self.showFaces(response.data);
+		     });
+		   } else {
+		     console.log('User cancelled login or did not fully authorize.');
+		   }
+		 }, {scope: ''});
 		
 	};
 	
+	this.showFaces = function(facebookIds) {
+		facesWrapper.empty();
+		for (var i = 0; i < facebookIds.length; i++) {
+			self.showFace(facebookIds[i]);
+		}
+	};
 	
-	this.showFace = function(facebookId) {
-		var fbId = encodeURIComponent(facebookId);
+	this.showPopup = function(facebookUser) {
+		popup.find('a').text(facebookUser.name).attr('href', 'http://www.facebook.com/profile.php?id=' + encodeURIComponent(facebookUser.id));
+		var relativeTo = $('.fb-' + facebookUser.id);
+		var relativeOffset = relativeTo.offset();
+		var width = relativeTo.width();
+		popup.css('top', relativeOffset.top);
+		popup.css('left', relativeOffset.left + width);
+		popup.show();
+
+	};
+	
+	
+	this.showFace = function(facebookUser) {
+		var fbId = encodeURIComponent(facebookUser.id);
+//		var wrapper = $('<div>', {'class': 'face'})
 		var img = $('<img/>', {src: 'http://graph.facebook.com/'+fbId+'/picture'});
 		img.addClass('fb-' + fbId);
 		img.hover(function(e) {
-			self.showPopup(fbId);
+			self.showPopup(facebookUser);
 		}, function(e) {
 			
 		});
