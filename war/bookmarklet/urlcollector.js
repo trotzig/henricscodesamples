d3ba55babc4387ef3c807bd1cf71d7f176d7f89a
Henric <henric.persson@gmail.com>
2012-01-31 22:44:25 +0100

    First commit, basic support for collecting urls

---
+++ war/bookmarklet/urlcollector.js
@@ -0,0 +1,28 @@
+/**
+ * javascript:(function(){
+  _my_script=document.createElement('SCRIPT');
+  _my_script.type='text/javascript';
+  _my_script.src='http://mysite.com/script.js?';
+  document.getElementsByTagName('head')[0].appendChild(_my_script);
+  })();
+ */
+
+
+function trotzig_URLCollector() {
+
+	this.collectCurrentURL = function() {
+		var url = window.location.href;
+		var xdomain = document.createElement('script');
+		xdomain.type='text/javascript';
+//		var host = 'http://localhost:8888';
+		var host = 'http://faces.trotzigprojects.appspot.com';
+		xdomain.src = host + '/collect?callback=trotzig_URLCollectorSuccess&url=' + encodeURIComponent(url);
+		document.getElementsByTagName('head')[0].appendChild(xdomain);
+	};
+
+};
+new trotzig_URLCollector().collectCurrentURL();
+function trotzig_URLCollectorSuccess(msg){
+	alert(msg);
+}
+ 
